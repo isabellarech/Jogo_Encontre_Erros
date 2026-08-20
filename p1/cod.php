0aed864ac155d00fd901a44e4710b972261bf04b
Isabella <isabella_rech@estudante.sesisenai.org.br>
2026-08-20 12:21:32 -0300

    ADD - Erro 1: Falta de ponto e vírgula na mensagem de erro de conexão

--- p1/cod.php
+++ p1/cod.php
@@ -8,7 +8,7 @@ $database = "crud_aula";
 $conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
-    die("Erro na conexão: " . $conn->connect_error)
+    die("Erro na conexão: " . $conn->connect_error);
 }
 
 // CADASTRAR
